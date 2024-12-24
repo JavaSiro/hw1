e5c156e339a66153dc6d0e21f7958d56470384da
JavaSiro <javokhirsirojiddinov@gmail.com>
2024-12-24 19:34:11 +0900

    .

---
+++ boolean.py
@@ -0,0 +1,6 @@
+first=float(input('We will compare if the numbers r equal or not\nType in the first number: '))
+second=float(input('type in the second number: '))
+if first ==second:
+    print(True)
+elif first!=second:
+    print(False)
--- integer.py
+++ integer.py
@@ -1,12 +1,13 @@
+#       data type detector      #
 first=1
 second=2
 third=3
 fourth=4
-fifth='sup'
-print(type(fifth))
+fifth=5
+print(type(first), type(fifth))
 
 
-
-# multiply_1=int(input("type in first number u wanna multiply:\n"))
-# multiply_2=int(input("type in second number u wanna multiply:\n"))
-# print('ur result is ', multiply_1 * multiply_2)
+#       multiplier      #
+multiply_1=int(input("type in first number u wanna multiply:\n"))
+multiply_2=int(input("type in second number u wanna multiply:\n"))
+print('ur result is ', multiply_1 * multiply_2)
