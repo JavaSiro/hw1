f542532be5d96a94adaa42bc8403784f4d885b3b
JavaSiro <javokhirsirojiddinov@gmail.com>
2024-12-24 20:23:19 +0900

    .

---
+++ if.py
@@ -0,0 +1,4 @@
+converter_choise=float(input('choose a converter:\n1. kg to pound\n2. pound to kg\n3. celsius to fahranheit\n4. fahranheit to celsius\n'))
+if converter_choise ==1:
+    first_converter=float(input('type how many kg u wanna convert into pound:\n'))
+    print(f"{first_converter} kg is {first_converter*2.20462} pounds")
---
+++ string.py
@@ -0,0 +1,3 @@
+name = input('type ur name: ').upper()
+family_name = input('type ur family name: ').upper()
+print('Welcome', name, family_name)
