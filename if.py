bdeb6c5f5066ba4343c46fce4cb1f74e5bbdb659
JavaSiro <javokhirsirojiddinov@gmail.com>
2024-12-26 20:53:07 +0900

    .

--- if.py
+++ if.py
@@ -2,3 +2,4 @@ converter_choise=float(input('choose a converter:\n1. kg to pound\n2. pound to k
 if converter_choise ==1:
     first_converter=float(input('type how many kg u wanna convert into pound:\n'))
     print(f"{first_converter} kg is {first_converter*2.20462} pounds")
+elif converter_choise==2:
